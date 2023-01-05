781a00714564427198ebfdb1909c3800abb301a6
Crapez <crapez.mina@gmail.com>
2023-01-05 10:56:28 +0100

    le serveur se lance. Les premieres informations s ecrivent

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # conception_applications_reparties
+
+## Mina Crapez
+
+
+-------------
+
+a faire : créer le serveur 
+le serveur doit pouvoir permettre de s'identifier et de rentrer son mdp
+penser à faire les min et max de threads 
+
+```
+javac ServeurFTP.java
+
+java ServeurFTP
+```
+
+``` 
+ftp ...
+```
--- ServeurFTP.java
+++ ServeurFTP.java
@@ -1,4 +1,69 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ServeurFTP {
 
+    private Socket socket;
+    private ArrayList<ServeurFTP> threads;
 
+    public ServeurFTP(Socket socket, ArrayList<ServeurFTP> threads){
+        this.socket = socket;
+        this.threads = threads;
+    }
+
+    public static void main(String[] args) throws IOException{
+        // initialisation
+        ArrayList<ServeurFTP> threads = new ArrayList<>(); 
+
+        // connexion
+        ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0])); 
+        Socket socket = serverSocket.accept(); 
+       
+        ServeurFTP server = new ServeurFTP(socket,threads);
+        
+        threads.add(server); 
+        server.start(); 
+
+        // on ferme les connexion
+        serverSocket.close();
+        socket.close(); 
+    }
+
+    public void start() throws IOException {
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+    
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        DataOutputStream dos = new DataOutputStream(os);
+
+        // pour tous les threads :
+        for(int i = 0; i < this.threads.size();i++){
+            dos.writeBytes("connected to localHost \r\n");
+            dos.writeBytes("Connect to host S, port L,\n establishing control connections.\n <---- 220 Service ready <CRLF>.");
+        }
+    } 
 }
+
+/*
+-----------------
+
+
+var serv  = new ServerSocket(4242);
+
+while (true) {
+    Socket s = serv.accept();
+
+    InputStream is = s.getInputStream();
+    OutputStream os = s.getOutputStream();
+
+
+    InputStreamReader isr = new inputStreamReader(is);
+    BufferedReader br = new BufferedReader(isr);
+    DataOutputStream dos = new DataOutputStream(os);
+
+    doc.writeBytes("connected to localHost");
+    
+} */
