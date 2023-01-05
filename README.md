24453ce2f791e9c24cce6f4473622cf9c1cf87d5
Crapez <crapez.mina@gmail.com>
2023-01-05 13:48:10 +0100

    avancement authentification et mdp, refactoring du code pour sous fonctions

--- README.md
+++ README.md
@@ -6,17 +6,21 @@
 -------------
 
 a faire : 
-- créer le serveur 
+
 - le serveur doit pouvoir permettre de s'identifier et de rentrer son mdp 
+
 - penser à faire les min et max de threads  
 - faire un readme propre
+- attention a gerer le cas ou on met pas de port en lançant le serv, en mettre un par défaut
+- mettre les mess derreur d'authentification et mdp
+- ameliorer la map de MDP pq la cest moche en brut comme ca 
 
 ```
 javac ServeurFTP.java
 
-java ServeurFTP
+java ServeurFTP 1024
 ```
 
 ``` 
-ftp ...
+ftp localhost 1024
 ```
--- ServeurFTP.java
+++ ServeurFTP.java
@@ -1,21 +1,47 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 
 public class ServeurFTP {
 
 
-
+    private DataOutputStream dos;
+    private BufferedReader br;
+    private InputStreamReader isr;
     private Socket socket;
     private ArrayList<ServeurFTP> threads;
     private int port;
+    private boolean isOpen;
+
+    Map<String, String> mdp;
 
     public ServeurFTP(Socket socket, ArrayList<ServeurFTP> threads, int port){
         this.socket = socket;
         this.threads = threads;
         this.port = port;
+        this.isOpen = true;
+        this.mdp = new HashMap<String, String>();
+        this.mdp.put("test","test");
+
+        try {
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+    
+            this.isr = new InputStreamReader(is);
+            this.br = new BufferedReader(isr);
+            this.dos = new DataOutputStream(os);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void write (String message) throws IOException {
+        dos.writeBytes(message);
     }
 
     public static void main(String[] args) throws IOException{
@@ -37,26 +63,41 @@ public class ServeurFTP {
     }
 
     public void start() throws IOException {
-
-        InputStream is = socket.getInputStream();
-        OutputStream os = socket.getOutputStream();
-    
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        DataOutputStream dos = new DataOutputStream(os);
-
-        // pour tous les threads :
         for(int i = 0; i < this.threads.size();i++){
             dos.writeBytes("Connect to host linux port "+this.port+ " establishing control connections.<---- 220 Service ready <CRLF>.");
-            
-            String msg = br.readLine();
-            Mdp mdp;
-            if(mdp.getMdp().containsValue(msg)){
-                dos.
-            }
+            while(isOpen) {
+                String msg = br.readLine();
+                this.authentification(msg);
+                this.motDePasse(msg);
+                }
+           }
         } 
+     
 
+    public void authentification(String message) throws IOException {
+        if(message.contains("username")){
+            String user = message.replace("username ","");
+            if(this.mdp.containsKey(user)) {
+                write("USER"+user+"<CRLF>----> <---- 331 User name ok, need password<CRLF>.");
+            }
+            else{
+                write("TROUVER LE CODE ERREUR");
+            }
+        }
+    } 
+
+    public void motDePasse(String message) throws IOException {
+        if(message.contains("password")){
+            String mdp = message.replace("password ","");
+            if(this.mdp.containsValue(mdp)) {
+                write("PASS"+mdp+"<CRLF>----><---- 230 User logged in<CRLF>.");
+            }
+            else{
+                write("TROUVER LE CODE ERREUR");
+            }
+        }
     }
+
 }
 
 /*
