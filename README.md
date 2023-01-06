15006ff3a9c4432a7f84b9267038234ea3ea8c2f
Crapez <crapez.mina@gmail.com>
2023-01-06 20:55:08 +0100

    correction bug d'affichage, ajout fonction write et correction mineur

--- README.md
+++ README.md
@@ -7,20 +7,27 @@
 
 a faire : 
 
-- le serveur doit pouvoir permettre de s'identifier et de rentrer son mdp 
+- le serveur doit pouvoir permettre de s'identifier et de rentrer son mdp
+bug au while (true), le premier print se fait sans soucis mais on a toujours le retour "invalid command" en voulant entrer du texte apres. 
 
-- penser à faire les min et max de threads  
+
+amelioration a faire :
+- penser à faire les min et max de threads et continuer leur gestion (en commentaire pour le moment)
 - faire un readme propre
-- attention a gerer le cas ou on met pas de port en lançant le serv, en mettre un par défaut
 - mettre les mess derreur d'authentification et mdp
 - ameliorer la map de MDP pq la cest moche en brut comme ca 
+- faire une FTPexception
 
+1er terminal :
 ```
 javac ServeurFTP.java
 
-java ServeurFTP 1024
+java ServeurFTP 1024 
+OU
+java ServeurFTP
 ```
 
+2eme terminal :
 ``` 
 ftp localhost 1024
 ```
--- ServeurFTP.java
+++ ServeurFTP.java
@@ -2,59 +2,49 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
-import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
-
 
 public class ServeurFTP {
 
+    protected Socket socket;
+    protected int port;
+    protected Boolean isOpen;
+   // private ArrayList<ServeurFTP> threads;
+    protected BufferedReader br;
+    protected PrintWriter printer;
+    protected Map<String, String> mdp;
+    
 
-    private DataOutputStream dos;
-    private BufferedReader br;
-    private InputStreamReader isr;
-    private Socket socket;
-    private ArrayList<ServeurFTP> threads;
-    private int port;
-    private boolean isOpen;
-
-    Map<String, String> mdp;
-
-    public ServeurFTP(Socket socket, ArrayList<ServeurFTP> threads, int port){
+    public ServeurFTP(Socket socket, int port) throws IOException{
         this.socket = socket;
-        this.threads = threads;
+        // this.threads = threads;
         this.port = port;
         this.isOpen = true;
+
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        OutputStream os = socket.getOutputStream();
+        this.br = new BufferedReader(isr);
+
+        this.printer = new PrintWriter(os,true);
+
         this.mdp = new HashMap<String, String>();
         this.mdp.put("test","test");
-
-        try {
-            InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();
-    
-            this.isr = new InputStreamReader(is);
-            this.br = new BufferedReader(isr);
-            this.dos = new DataOutputStream(os);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-    }
-
-    public void write (String message) throws IOException {
-        dos.writeBytes(message);
     }
 
     public static void main(String[] args) throws IOException{
         // initialisation
-        ArrayList<ServeurFTP> threads = new ArrayList<>(); 
-        int port = Integer.parseInt(args[0]);
+        //ArrayList<ServeurFTP> threads = new ArrayList<>(); 
+        int port = 1024;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
         // connexion
         ServerSocket serverSocket = new ServerSocket(port); 
         Socket socket = serverSocket.accept(); 
-       
-        ServeurFTP server = new ServeurFTP(socket,threads,port);
-        
-        threads.add(server); 
+
+        ServeurFTP server = new ServeurFTP(socket,port);
+
+        //threads.add(server); 
         server.start(); 
 
         // on ferme les connexion
@@ -62,20 +52,25 @@ public class ServeurFTP {
         socket.close(); 
     }
 
+    public void write (String message) {
+        printer.println(message);
+    }
+
     public void start() throws IOException {
-        for(int i = 0; i < this.threads.size();i++){
-            dos.writeBytes("Connect to host linux port "+this.port+ " establishing control connections.<---- 220 Service ready <CRLF>.");
-            while(isOpen) {
-                String msg = br.readLine();
-                this.authentification(msg);
-                this.motDePasse(msg);
-                }
-           }
+        // pour tous les threads :
+        //  for(int i = 0; i < this.threads.size();i++){
+        write("Connect to host Linux, port "+port+",establishing control connections. <---- 220 Service ready <CRLF>.");
+
+        while (true) { // !! ne marche pas !!
+            String msg = br.readLine();
+            authentification(msg);
+            motDePasse(msg);
         } 
-     
+
+    } 
 
     public void authentification(String message) throws IOException {
-        if(message.contains("username")){
+        if(message.startsWith("username")){
             String user = message.replace("username ","");
             if(this.mdp.containsKey(user)) {
                 write("USER"+user+"<CRLF>----> <---- 331 User name ok, need password<CRLF>.");
@@ -87,7 +82,7 @@ public class ServeurFTP {
     } 
 
     public void motDePasse(String message) throws IOException {
-        if(message.contains("password")){
+        if(message.startsWith("password")){
             String mdp = message.replace("password ","");
             if(this.mdp.containsValue(mdp)) {
                 write("PASS"+mdp+"<CRLF>----><---- 230 User logged in<CRLF>.");
@@ -97,26 +92,7 @@ public class ServeurFTP {
             }
         }
     }
-
 }
 
-/*
------------------
 
 
-var serv  = new ServerSocket(4242);
-
-while (true) {
-    Socket s = serv.accept();
-
-    InputStream is = s.getInputStream();
-    OutputStream os = s.getOutputStream();
-
-
-    InputStreamReader isr = new inputStreamReader(is);
-    BufferedReader br = new BufferedReader(isr);
-    DataOutputStream dos = new DataOutputStream(os);
-
-    doc.writeBytes("connected to localHost");
-    
-} */
