fe7014d54c461d23ef15ad3100d8946c4ad0feea
Mina Crapez <79222311+MinaCrapez@users.noreply.github.com>
2023-03-31 13:47:02 +0200

    refactoring reducerActor et correction du comptage des mots

--- mapReduce/src/main/java/tp3/akka/mapReduce/actor/ReducerActor.java
+++ mapReduce/src/main/java/tp3/akka/mapReduce/actor/ReducerActor.java
@@ -1,32 +1,32 @@
 package tp3.akka.mapReduce.actor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import akka.actor.ActorRef;
+import akka.actor.RepointableActorRef;
 import akka.actor.UntypedActor;
-import tp3.akka.mapReduce.message.Reducer;
+import tp3.akka.mapReduce.message.ReducerMessage;
 
 public class ReducerActor  extends UntypedActor {
 	
-	private Map<String, Integer> compteur;
+	private Map<String, Integer> compteur = new HashMap<>();
 	
 	@Override
 	public void onReceive(Object message) throws Exception {
-		if (message instanceof Reducer) {
-			Reducer reducer = (Reducer) message;
-			String mot = reducer.getMot();
-			compteur = reducer.getCompteur();
-			if (compteur.containsKey(mot)) { // si le mot est déjà dans le compteur, on incrémente le compteur
-				int interation = compteur.get(mot) + 1;
-				compteur.remove(mot);
-				compteur.put(mot, interation);
-			}
-			else { // sinon, on l'initialise
-				compteur.put(mot, 1);
-			}
+		if (message instanceof ReducerMessage) {
+			ReducerMessage reducerMessage = (ReducerMessage) message;
+			String mot = reducerMessage.getMot();
+			int count = compteur.getOrDefault(mot, 0);
+			compteur.put(mot, count + 1);
 		}
 		else if(message instanceof String) {
-			this.getSender().tell(compteur, ActorRef.noSender());
+			String mot = (String) message;
+			int count = compteur.getOrDefault(mot, 0);
+			this.getSender().tell(count, ActorRef.noSender());
+		}
+		else {
+			return;
 		}
 	}
 
