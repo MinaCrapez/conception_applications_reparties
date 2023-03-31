c400819e545abd50ce3bbb6e8cf2a397d5e0b69d
Mina Crapez <79222311+MinaCrapez@users.noreply.github.com>
2023-03-31 13:45:28 +0200

    refactoring du akkaService

--- mapReduce/src/main/java/tp3/akka/mapReduce/service/AkkaService.java
+++ mapReduce/src/main/java/tp3/akka/mapReduce/service/AkkaService.java
@@ -57,28 +57,20 @@ public class AkkaService {
 		ActorRef[] mappers = {mapper1,mapper2,mapper3};
 		int i = 0;
 		while( ((ligne=br.readLine()) != null)){ // tant qu'il reste des lignes au fichier
-			mappers[i].tell(new Mapper(ligne,reducer1,reducer2), ActorRef.noSender());
+			mappers[i].tell(new MapperMessage(ligne,reducer1,reducer2), ActorRef.noSender());
 			i = (i + 1) % 3;
 		}
 	}
 	
-	// methode qui retourne le nombre d'occurence d'un mot
+		// methode qui retourne le nombre d'occurence d'un mot
 	public int compteMot(String mot) {
-		int res;
 		String moitieAlphabet = "abcdefghijklm";
 		String premiereLettre = mot.substring(0,1).toLowerCase();
 		Inbox inbox = Inbox.create(system);
-		 if (moitieAlphabet.contains(premiereLettre)) { 
-			inbox.send(reducer1,mot);
-			Object compteur = inbox.receive(FiniteDuration.create(5,TimeUnit.SECONDS)); 
-			res = ((Map<String, Integer>) compteur).get(mot);
-		 } 
-		 else { 
-			 inbox.send(reducer2,mot);
-			 Object compteur = inbox.receive(FiniteDuration.create(5,TimeUnit.SECONDS)); 
-			 res = ((Map<String, Integer>) compteur).get(mot);
-		 }
-		 return res;
+		if (moitieAlphabet.contains(premiereLettre)) inbox.send(reducer1,mot);
+		else inbox.send(reducer2,mot);
+		Object count = inbox.receive(FiniteDuration.create(5,TimeUnit.SECONDS)); 
+		return (int) count;
 	}
 }
 
